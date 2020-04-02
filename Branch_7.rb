e2df5b246bb93089f134b65ac8f3d2d8ca1af08a
cougarsyankeesfan <cougarsyankeesfan@users.noreply.github.com>
2020-04-01 23:27:41 -0600

    Add files via upload

---
+++ Branch_7.rb
@@ -0,0 +1,11 @@
+puts 'Hello, what\'s your name?'
+
+name = gets.chomp
+
+puts 'Hello, ' + name + '.'
+
+if name == 'Chris'
+
+    puts 'What a lovely name!'
+
+end
---
+++ CompareM_7.rb
@@ -0,0 +1,27 @@
+puts 1 > 2
+
+puts 1 < 2
+
+puts 5 >= 5
+
+puts 5 <= 4
+
+puts 4 == 4
+
+puts 1 == 2
+
+puts 2 != 1
+
+puts 'Rollo' > 'John'
+
+puts 'cat' < 'dog'
+
+puts 'cat' > 'dog'
+
+puts 'bug lady' < 'Xander'
+
+puts 'bug lady'.downcase < 'Xander'.downcase
+
+puts 1 < 80
+
+puts '7' < '80'
